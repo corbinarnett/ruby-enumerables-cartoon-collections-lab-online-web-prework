2958a88f804edd2b9ca294825a4dbc86a58cc6a3
Corbin Arnett <corbin.c.arnett@gmail.com>
2019-06-15 04:25:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,8 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  array.find { |item|  }
+  array.find { |item|
+  cheese_types.include?(item)
+  }
 
 end
